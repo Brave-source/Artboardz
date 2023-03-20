be62ec9623330ddf919ac559758120e30b154fb9
gero <gerorubio@outlook.com>
2023-03-20 17:14:38 -0600

    Initial dropdown connect wallet

--- src/components/ConnectWallet/ConnectWallet.jsx
+++ src/components/ConnectWallet/ConnectWallet.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { useWallet, useWalletList } from '@meshsdk/react'
 // import { BsChevronDown } from 'react-icons/bs'
 import Image from 'next/image'
-import { Button } from '@mui/material'
+import { Button, Menu, MenuItem } from '@mui/material'
 import Dropdown from './Dropdown'
 
 const ConnectWallet = () => {
@@ -36,7 +36,78 @@ const ConnectWallet = () => {
         disconnect()
         setSelectedWallet(null)
     }
-return (
+
+    
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+  
+
+
+    return (
+        <>
+        <div>
+            <Button
+                id="basic-button"
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+            >
+                {selectedWallet ? (
+                    <div className='flex'>
+                        <div className='bg-orange-500'>logo</div>
+                        <div className='bg-emerald-400'>balance</div>
+                    </div>
+                ) : connecting ? (
+                    <div>Connecting</div>
+                ) : (
+                    <div>Connect</div>
+                )}
+            </Button>
+                {selectedWallet ? (
+                    <div className='flex'>
+                        <div className='bg-orange-500'>logo</div>
+                        <div className='bg-emerald-400'>balance</div>
+                    </div>
+                ) : connecting ? (
+                    <div>Connecting</div>
+                ) : (
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                        'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                        {wallets.map((wallet, index) => {
+                            <MenuItem key={index} onClick={() => handleWalletSelection(wallet)}>
+                                dandjna
+                                {wallet.name}
+                                <Image
+                                    src={wallet.icon}
+                                    alt={wallet.name}
+                                    width='30'
+                                    height='30'
+                                />
+                            </MenuItem>
+                        })}
+                    </Menu>
+                )}
+        </div>
+
+
+
+
+
         <Dropdown
             title={
                 <Button >
@@ -92,6 +163,7 @@ return (
                 )}
             </div>
         </Dropdown>
+        </>
     )
 }
 
