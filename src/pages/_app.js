70797f46f5015cc6f050de3dec45a8ba2ecd82dc
Covenant Chukwudi <covenantchukwudi@gmail.com>
2023-03-24 06:30:30 -0500

    testing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -43,7 +43,7 @@ function MyApp({ Component, pageProps }) {
     const getCollections =async() => {
       dispatch(getCollectionStart())
       try {
-        const res = await axios.get('http://3.230.126.26/api/collections');
+        const res = await axios.get('http://54.159.18.143:3000/api/collections');
         dispatch(getCollectionSuccess(res.data))
       }catch(err) {
         dispatch(getCollectionFailure())
@@ -56,7 +56,7 @@ function MyApp({ Component, pageProps }) {
     const getCollections =async() => {
       dispatch(getCollectorStart())
       try {
-        const res = await axios.get('http://3.230.126.26/api/users');
+        const res = await axios.get('http://54.159.18.143:3000/api/users');
         dispatch(getCollectorSuccess(res.data))
       }catch(err) {
         dispatch(getCollectorFailure())
