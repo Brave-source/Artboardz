d56c3d6f7ff8cbeb7b81a244df5487ac4dcf0b55
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-04-15 19:08:07 +0100

    update function edited

--- src/pages/api/edit/update.js
+++ src/pages/api/edit/update.js
@@ -9,29 +9,29 @@ const updateNFTs = async() => {
     const users = await User.find();
     
     for(let user of users) {
-        const storeAssets = user.assets.map((asset) => { return asset.unit});
-        const storedPolicyIds = user.policyIds;
+        const storeAssets = user.assets?.map((asset) => { return asset?.unit});
+        const storedPolicyIds = user?.policyIds;
 
-        let res = await getNFTByAddress(user.stakeAddress);
-        const assets = res.amount.map((asset) => asset.unit);
-        const filteredAssets = assets.filter((asset) => asset !== "lovelace");
+        let res = await getNFTByAddress(user?.stakeAddress);
+        const assets = res?.amount?.map((asset) => asset?.unit);
+        const filteredAssets = assets?.filter((asset) => asset !== "lovelace");
         
         let policyIds = [];
         let units = [];
 
         await Promise.all(
-            filteredAssets.map(async(item) => {
+            filteredAssets?.map(async(item) => {
                 const block = await getNFTsForAddress(item);
                 return policyIds.push(block.policy_id), units.push(block.asset)
             })
         )
        const newPolicyIds = [].concat(...new Set(policyIds));
        const newUnits = [].concat(...new Set(units));
-       const filteredUnits = storeAssets.filter((item) => !newUnits.includes(item));
+       const filteredUnits = storeAssets?.filter((item) => !newUnits?.includes(item));
        
-       const filteredPolicyIds = storedPolicyIds.filter((item) => !newPolicyIds.includes(item));
+       const filteredPolicyIds = storedPolicyIds?.filter((item) => !newPolicyIds?.includes(item));
        await Promise.all(
-        filteredPolicyIds.map(async(policy_id) => {
+        filteredPolicyIds?.map(async(policy_id) => {
             await User.findByIdAndUpdate(user._id, {
                 $pull : {
                     policyIds: policy_id
@@ -46,7 +46,7 @@ const updateNFTs = async() => {
         })
        )
       await Promise.all(
-        filteredUnits.map(async(unit) => {
+        filteredUnits?.map(async(unit) => {
             await User.findByIdAndUpdate(user._id, {
                 $pull: {
                     assets: {
