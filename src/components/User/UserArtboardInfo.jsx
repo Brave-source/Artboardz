1b78a502b4df9fa5225f4ecc602ad973c9c45087
Paulo Diaz <paulodiazg32@gmail.com>
2023-04-19 11:57:21 -0700

    User Artboardz fix

--- src/components/User/UserArtboardInfo.jsx
+++ src/components/User/UserArtboardInfo.jsx
@@ -10,16 +10,16 @@ const UserArtboardzInfo = ({ name, jpgLink }) => {
     <div className="w grid grid-cols-1 space-y-1 tracking-wide text-white font-Montserrat content-evenly">
       <div className="flex flex-row gap-1 items-center justify-between">
         <p className="text-sm font-semibold ">{name?.slice(0,20)}</p>
-        <div className='flex flex-row gap-x-2'>
+        {/* <div className='flex flex-row gap-x-2'>
         <Image src={CardanoIcon} height={20} unoptimized={true}></Image>
-        </div>
+        </div> */}
         
         {/* <p className="text-sm">Listings: 3/30</p> */}
       </div>
       <div className="flex gap-1 items-center justify-between">
         <a href={jpgLink} target="_blank" rel="noreferrer">
-         <button className={ "mt-[5px] bg-active-link rounded-xl font-semibold w-full h-[30px]  mx-auto block tracking-wide text-base"}>
-            Jpgstore
+         <button className={ "mt-[5px] bg-active-link rounded-xl font-semibold w-full h-[30px]  px-2 mx-auto block tracking-wide text-base"}>
+            Trade
           </button>
         </a>
       </div>
