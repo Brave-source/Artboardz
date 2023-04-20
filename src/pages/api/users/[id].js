38fb82b086bd7f918616ea47b87b46c01deb1cd8
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-04-20 15:01:47 +0100

    still solving backend issues for user id

--- src/pages/api/users/[id].js
+++ src/pages/api/users/[id].js
@@ -27,7 +27,6 @@ export default async function handler(req, res) {
 
     try {
       const user = await User.findById(req.body.id);
-      console.log(req.body.units)
      if(user) {
       const policyId = req.body.policyIds;
         let ass = [];
@@ -43,7 +42,7 @@ export default async function handler(req, res) {
               quantity: block.quantity
             })
             }catch(err) {
-              
+
             }
           })
         )
