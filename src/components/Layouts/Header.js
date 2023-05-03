c87e44ce0e388ed6c90d63ccb117723e2eb32b57
Aondohemba Iorolun Emmanuel <emmyaondohemba45@gmail.com>
2023-05-03 05:18:18 +0100

    change back to mesh js

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -25,14 +25,14 @@ import { getNFTByAddress } from "../blockfrost/Blockfrost";
 
 const Header = () => {
   const dispatch = useDispatch();
-  // const address = useAddress();
+  const address = useAddress();
   const [image, setImage] = useState("");
   const [isUser, setIsUser] = useState(false);
   const network = useNetwork();
   const assets = useAssets();
   const { connected } = useWallet();
   const user = useSelector((item) => item.user.user);
-  const address = useSelector((item) => item.user.walletAddress);
+  // const address = useSelector((item) => item.user.walletAddress);
   const id = user._id;
   const  isMainnet = useSelector((item) => item.collector.isMainnet);
 
