bfb3dbc6fbe0f636aeb913fba064c1359a91c2ac
Paulo Diaz <paulodiazg32@gmail.com>
2023-06-03 16:14:37 -0700

    Mapz

--- src/components/sections/MapzSection/MapzSection.jsx
+++ src/components/sections/MapzSection/MapzSection.jsx
@@ -1,34 +1,34 @@
 
-import React, { useState } from 'react';
-let Globe = () => null;
-if (typeof window !== "undefined") Globe = require("react-globe.gl").default;
+// import React, { useState } from 'react';
+// let Globe = () => null;
+// if (typeof window !== "undefined") Globe = require("react-globe.gl").default;
 
 
     
-    // import optional tippy styles for tooltip support
-    // import 'tippy.js/dist/tippy.css';
-    // import 'tippy.js/animations/scale.css';
-const MapzSection = () => {
-    const [imageUrl, setImageUrl] = React.useState("/images/texture.png");
+//     // import optional tippy styles for tooltip support
+//     // import 'tippy.js/dist/tippy.css';
+//     // import 'tippy.js/animations/scale.css';
+// const MapzSection = () => {
+//     const [imageUrl, setImageUrl] = React.useState("/images/texture.png");
 
     
-  return(
-    <Globe
-    //@ts-ignore
+//   return(
+//     <Globe
+//     //@ts-ignore
 
-    width={480}
-    height={480}
-    backgroundColor={"rgba(0,0,0,0)"}
-    globeImageUrl={imageUrl}
-    arcColor={"color"}
-    arcsData={arcsData}
-    arcDashGap={0.6}
-    arcDashLength={0.3}
-    arcDashAnimateTime={4000 + 500}
-    rendererConfig={{ preserveDrawingBuffer: true }}
-  />)
+//     width={480}
+//     height={480}
+//     backgroundColor={"rgba(0,0,0,0)"}
+//     globeImageUrl={imageUrl}
+//     arcColor={"color"}
+//     arcsData={arcsData}
+//     arcDashGap={0.6}
+//     arcDashLength={0.3}
+//     arcDashAnimateTime={4000 + 500}
+//     rendererConfig={{ preserveDrawingBuffer: true }}
+//   />)
     
   
-}
+// }
 
-export default MapzSection
+// export default MapzSection
