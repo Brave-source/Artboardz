ab64a74e8afaba50b057af8d067e3fcbb2c0511b
Brave-source <eebiyenrin@gmail.com>
2023-07-07 13:44:30 +0100

    fix: wallet connect

--- src/components/ConnectWallet/ConnectWallet.jsx
+++ src/components/ConnectWallet/ConnectWallet.jsx
@@ -1,98 +1,104 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import { useState, useEffect, Fragment } from 'react'
 import { useWallet, useWalletList, useLovelace, useNetwork } from '@meshsdk/react'
 // import { BsChevronDown } from 'react-icons/bs'
-import LoginModal from './LoginModal';
 import Image from 'next/image'
-import { Button, Menu, MenuItem } from '@mui/material'
+import { Button, Menu, MenuItem, Typography } from '@mui/material'
 import { Lucid } from "lucid-cardano";
-import Dropdown from './Dropdown'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { getWalletAddress, logUserSuccess } from '../../store/redux-slices/userSlice';
 import { useDispatch } from 'react-redux';
+import LoginModal from './LoginModal';
+import HeaderProfile from '../User/HeaderProfile';
+import { ChevronDownIcon, ChevronUpIcon, PowerIcon, UserIcon, WalletIcon } from '@heroicons/react/24/solid';
 
 const ConnectWallet = () => {
-    const { wallet, connected, connect, disconnect, connecting } = useWallet()
-    const wallets = useWalletList()
-    const lovelace = useLovelace();
-    const [isOpen, setIsOpen] = useState(false)
-    const [selectedWallet, setSelectedWallet] = useState(null)
-    const network = useNetwork();
-    const lovelaceAssets = parseInt(lovelace) / 1000000;
-    const dispatch = useDispatch();
-    
-    useEffect(() => {
-        const storedWallet = localStorage.getItem('selectedWallet');
-        const address = JSON.parse(localStorage.getItem("walletAddress"));
-        if (storedWallet) {
-            setSelectedWallet(JSON.parse(storedWallet))
-            connect(JSON.parse(storedWallet)?.name);
-        }
-        if(address) {
-            LuciConnectWalletAddress(JSON.parse(storedWallet)?.name);
-        }
-    }, [])
-    const walletList = ["eternl", "Nami", "GeroWallet", "Flint Wallet"]
-    const filteredWallets = wallets.filter((wallet) => walletList.includes(wallet?.name));
-    const LuciConnectWalletAddress = async(name) => {
-        let api;
-        switch(name) {
-            case "eternl":
-                api = await window.cardano?.eternl.enable();
-                break;
-            case "Nami":
-                api = await window.cardano.nami.enable();
-                break;
-            case "Flint Wallet":
-                api = await window.cardano.flint.enable();
-                break;
-            // case "Typhon Wallet":
-            //     api = await window.cardano.typhon.enable();
-            //     break;
-            // case "Yoroi":
-            //     api = await window.cardano.yoroi.enable();
-            //     break;
-            case "GeroWallet":
-                api = await window.cardano.gerowallet.enable();
-                break;
-            default:
-                                                                                     
-        }
-        const  lucid = await Lucid.new();
-        lucid.selectWallet(api);
-        const address = await lucid.wallet.address();
-        localStorage.setItem("walletAddress", JSON.stringify(address));
-        dispatch(getWalletAddress(address));
-    }
+  const { wallet, connected, connect, disconnect, connecting } = useWallet()
+  const wallets = useWalletList()
+  const lovelace = useLovelace();
+  const [isOpen, setIsOpen] = useState(false)
+  const [selectedWallet, setSelectedWallet] = useState(null)
+  const network = useNetwork();
+  const lovelaceAssets = parseInt(lovelace) / 1000000;
+  const dispatch = useDispatch();
 
-    const handleWalletSelection = (wallet) => {
-        if (network == 0) {
-          alert('This dapp only works on Cardano Mainnet')
-          return
-        } 
-        localStorage.setItem('selectedWallet', JSON.stringify(wallet))
-        setSelectedWallet(wallet)
-        connect(wallet.name)
-        setIsOpen(false)
-        LuciConnectWalletAddress(wallet.name);
-      }
-
-    const handleDisconnect = () => {
-        localStorage.removeItem('selectedWallet');
-        localStorage.removeItem("walletAddress");
-        disconnect()
-        setSelectedWallet(null)
-        dispatch(logUserSuccess());
+  useEffect(() => {
+    const storedWallet = localStorage.getItem('selectedWallet');
+    const address = JSON.parse(localStorage.getItem("walletAddress"));
+    if (storedWallet) {
+      setSelectedWallet(JSON.parse(storedWallet))
+      connect(JSON.parse(storedWallet)?.name);
     }
-    
-    // Dropdown handle
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
+    if(address) {
+      LuciConnectWalletAddress(JSON.parse(storedWallet)?.name);
+    }
+  }, [])
+
+  const walletList = ["eternl", "Nami", "GeroWallet", "Flint Wallet"]
+  const filteredWallets = wallets.filter((wallet) => walletList.includes(wallet?.name));
+  const LuciConnectWalletAddress = async(name) => {
+    let api;
+    switch(name) {
+      case "eternl":
+        api = await window.cardano?.eternl.enable();
+        break;
+      case "Nami":
+        api = await window.cardano.nami.enable();
+        break;
+      case "Flint Wallet":
+        api = await window.cardano.flint.enable();
+        break;
+      // case "Typhon Wallet":
+      //   api = await window.cardano.typhon.enable();
+      //   break;
+      // case "Yoroi":
+      //   api = await window.cardano.yoroi.enable();
+      //   break;
+      case "GeroWallet":
+        api = await window.cardano.gerowallet.enable();
+        break;
+      default:
+    }
+    const lucid = await Lucid.new();
+    lucid.selectWallet(api);
+    const address = await lucid.wallet.address();
+    localStorage.setItem("walletAddress", JSON.stringify(address));
+    dispatch(getWalletAddress(address));
+  }
+
+  const handleWalletSelection = (wallet) => {
+    if (network == 0) {
+      alert('This dapp only works on Cardano Mainnet')
+      return
+    }
+    localStorage.setItem('selectedWallet', JSON.stringify(wallet))
+    setSelectedWallet(wallet)
+    connect(wallet.name)
+    setIsOpen(false)
+    LuciConnectWalletAddress(wallet.name);
+  }
+
+  const handleDisconnect = () => {
+    localStorage.removeItem('selectedWallet');
+    localStorage.removeItem("walletAddress");
+    disconnect()
+    setSelectedWallet(null)
+    dispatch(logUserSuccess());
+  }
+
+  // Dropdown handle
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    if(connected && !connecting) {
       setAnchorEl(event.currentTarget);
+    } else {
+      setIsOpenLogin(!isOpenLogin);
+    }
     };
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
+  const handleClose = () => {
+    setAnchorEl(null);
+    setIsOpenNested(false)
+  };
 
   const handleCloseNested = () => {
   
@@ -193,7 +199,7 @@ const ConnectWallet = () => {
       }
       <LoginModal isOpen={isOpenLogin} setIsOpen={setIsOpenLogin} />
     </div>
-    )
+  )
 }
 
 export default ConnectWallet
